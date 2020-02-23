d6308afe0933616a047a58a6de2500188e526192
Lukcia <50925783+Lukcia@users.noreply.github.com>
2020-02-23 17:52:48 +0200

    Vizzy v0.3
    3 Labaratorinis

---
+++ script.js
@@ -0,0 +1,69 @@
+
+function ImageSwitch1()
+{
+document.body.style.backgroundColor = "#f3f3f3";
+document.body.style.backgroundImage = "url('front_page_PNG.png')";
+}
+
+function ImageSwitch2()
+{
+document.body.style.backgroundColor = "#f3f3f3";
+document.body.style.backgroundImage = "url('IMG1.jpg')";
+}
+
+function ImageSwitch3()
+{
+document.body.style.backgroundColor = "#f3f3f3";
+document.body.style.backgroundImage = "url('IMG2.jpg')";
+}
+
+function ImageSwitch4()
+{
+document.body.style.backgroundColor = "#f3f3f3";
+document.body.style.backgroundImage = "url('IMG3.jpg')";
+}
+
+
+
+function Vizzy()
+{
+    var dd1 = document.getElementById("paragraph");
+    var dd2 = document.getElementById("Maintext");
+    dd1.innerHTML = "Super simple tool to create trendy videos and pictures <br> for your ads, social media, blogs and website.<br/> Make your project stand out!";
+    dd2.innerHTML = "<b>Create Stunning</b> <br> <b>Visual Media</b>";
+
+}
+
+function Home()
+{
+    var dd1 = document.getElementById("paragraph");
+    var dd2 = document.getElementById("Maintext");
+   dd1.innerHTML = "Man galva skauda";
+   dd2.innerHTML = "o";
+}
+
+function Products()
+{
+    var dd1 = document.getElementById("paragraph");
+    var dd2 = document.getElementById("Maintext");
+   dd1.innerHTML = "Life is like a box of cholocates";
+   dd2.innerHTML = "i";
+}
+
+function Templates()
+{
+    var dd1 = document.getElementById("paragraph");
+    var dd2 = document.getElementById("Maintext");
+   dd1.innerHTML = "KekW";
+   dd2.innerHTML = "xd";
+}
+
+function Pricing()
+{
+    var dd1 = document.getElementById("paragraph");
+    var dd2 = document.getElementById("Maintext");
+   dd1.innerHTML = "ayyyyyyyyy";
+   dd2.innerHTML = "auyuuu";
+}
+
+
